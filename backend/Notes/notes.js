c4f91c56eb50de471e4e07483b24d3a7c4314c6c
Bishal Poudel <poudelb172@gmail.com>
2021-11-29 22:04:24 +0545

    notes create and read made

--- backend/Notes/notes.js
+++ backend/Notes/notes.js
@@ -9,7 +9,55 @@ const db = mysql.createConnection({
 })
 
 //create a note
+router.post('/create', (req, res) => {
+    db.query("INSERT into notesinfo (title,description,id) VALUES (?,?,?)",
+        [req.body.title,
+        req.body.description,
+        req.body.id],
+        (err, result) => {
+            if (err) {
+                res.status(500).json(err)
+            }
+            else {
+                res.status(200).json(result)
+            }
+        }
+    )
+})
+
+//getnotes of a user
+router.get('/getnotes', (req, res) => {
+    db.query("SELECT * FROM notesinfo WHERE id=?",
+        [req.body.id],
+        (err, result) => {
+            if (err) {
+                res.status(500).json(err)
+            }
+            else {
+                res.status(200).json(result)
+            }
+        }
+    )
+})
+
+//getnote by its id
+router.get('/:noteid', (req, res) => {
+    db.query("SELECT * FROM notesinfo WHERE note_id=?",
+        [req.params.noteid],
+        (err, result) => {
+            if (err) {
+                res.status(500).json(err)
+            }
+            else {
+                res.status(200).json(result)
+            }
+        }
+    )
+})
+
+//delete a note
 
 
+//edit a note
 
 module.exports = router
